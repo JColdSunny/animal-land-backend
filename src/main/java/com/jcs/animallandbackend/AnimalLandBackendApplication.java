7665b00f9fb60f2e357d483a7d5c28a8e4ef1b86
Dzmitry Bahaslouski <jcoldsun.tech@gmail.com>
2024-04-06 14:49:19 +0300

    init

---
+++ src/main/java/com/jcs/animallandbackend/AnimalLandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jcs.animallandbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalLandBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalLandBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcs/animallandbackend/AnimalLandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcs.animallandbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalLandBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
